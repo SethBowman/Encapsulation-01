789dbaffaf2a7d18715499d1619b91f6485d60e9
Seth Bowman <brandonsethbowman@gmail.com>
2021-10-11 10:55:47 -0500

    Exercise completed

---
+++ Encapsulation-01/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace EncapsulationInClass
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public BankAccount(double amount)
+        {
+            _balance = amount;
+        }
+
+        public double Deposit(double amount)
+        {
+            return _balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            return _balance -= amount;
+        }
+    }
+}
---
+++ Encapsulation-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncapsulationInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mAcct = new BankAccount(400);
+
+            var bal = mAcct.GetBalance();
+            Console.WriteLine("Balance:");
+            Console.WriteLine($"${bal}");
+
+            bal = mAcct.Deposit(2500);
+            Console.WriteLine("(+2500) Balance after deposit:");
+            Console.WriteLine($"${bal}");
+
+            bal = mAcct.Withdraw(250);
+            Console.WriteLine("(-250) Balance after withdrawal:");
+            Console.WriteLine($"${bal}");
+        }
+    }
+}
